a1e07b9a7806ee6c2375fd2625292c36ba43be32
ZevanRosser <bendvent@gmail.com>
2015-01-29 16:34:53 -0500

    basic prototype working

--- src/quick-shader.js
+++ src/quick-shader.js
@@ -1,4 +1,4 @@
-/*(function() {
+(function() {
   
   // requestAnimationFrame shim with setTimeout fallback by 
   // Paul Irish http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
@@ -33,78 +33,51 @@
         params.parentNode : document.querySelector(params.parentNode);
     }
     
-    this.init();
-    
- 
-    
+    this.init(); 
   };
   
   QuickShader.prototype = {
     
     constructor: QuickShader, 
     
-    init: function() {
+    init: function() { 
       var gl, vertices;
       
       this.canvas = document.createElement('canvas');
       this.canvas.width = this.width;
       this.canvas.height = this.height;
+      this.parentNode.appendChild(this.canvas);
+      
       gl = this.gl = this.ctx = this.canvas.getContext("experimental-webgl");
-      vertices = new Float32Array([ -1., -1.,   1., -1.,    -1.,  1.,     1., -1.,    1.,  1.,    -1.,  1.]);
+    
+      vertices = new Float32Array([ -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0 ]);
+      
       this.quadVBO = gl.createBuffer();
       gl.bindBuffer(gl.ARRAY_BUFFER, this.quadVBO);
       gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
       
-      console.log(this.parentNode);
-      
-      this.parentNode.appendChild(this.canvas);
       this.configureShader();
       this.size(this.width, this.height);
-      
-      this.render(0);
-      
-      // Main loop.
-      var self = this;
-      var t0 = (new Date()).getTime();  
-      function animate() {
-        self.render( ((new Date()).getTime()-t0)/1000 );
-        requestAnimationFrame( animate );
-      }
-      animate();
     },
     
     size: function(width, height) {
       this.width = width;
       this.height = height;
-      this.gl.viewport( 0, 0, width, height);
     },
     
-    configureShader: function() {
-      // console.log(this.shader);
+    configureShader : function()
+    {
+      
       var gl = this.gl,
           tmpProgram = gl.createProgram(),
           vs = gl.createShader(gl.VERTEX_SHADER),
           fs = gl.createShader(gl.FRAGMENT_SHADER);
       
-      gl.shaderSource(vs, "attribute vec2 pos;void main(){gl_Position=vec4(pos.x,pos.y,0.0,1.0);}");
+      gl.shaderSource(vs, "attribute vec2 pos;void main(){gl_Position=vec4(pos.x,pos.y,0.0,1.00);}");
       gl.shaderSource(fs, this.shader);
       
       gl.compileShader(vs);
       gl.compileShader(fs);
-      
-      if (!gl.getShaderParameter(vs, gl.COMPILE_STATUS))
-      {
-        var infoLog = gl.getShaderInfoLog(vs);
-        gl.deleteProgram( tmpProgram );
-        return "VS ERROR: " + infoLog;;
-      }
-      
-      if (!gl.getShaderParameter(fs, gl.COMPILE_STATUS))
-      {
-        var infoLog = gl.getShaderInfoLog(fs);
-        gl.deleteProgram( tmpProgram );
-        return "FS ERROR: " + infoLog;
-      }
       
       gl.attachShader(tmpProgram, vs);
       gl.attachShader(tmpProgram, fs);
@@ -114,41 +87,41 @@
       
       gl.linkProgram(tmpProgram);
       
-      if( this.mProgram != null ) gl.deleteProgram( this.mProgram );
+      if(this.shaderProgram) {
+        gl.deleteProgram( this.shaderProgram );
+      }
       
-      this.mProgram = tmpProgram;
- 
+      this.shaderProgram = tmpProgram;
     },
     
     render: function(time) {
       var gl = this.gl;
       
-      gl.viewport( 0, 0, this.mXres, this.mYres );
+      gl.viewport( 0, 0, this.width, this.height );
       
-      gl.useProgram(this.mProgram);
+      gl.useProgram(this.shaderProgram);
       
-      var l1 = gl.getAttribLocation(this.mProgram, "pos");
-      var l2 = gl.getUniformLocation(this.mProgram, "time");
-      var l3 = gl.getUniformLocation(this.mProgram, "resolution");
+      var l1 = gl.getAttribLocation(this.shaderProgram, "pos");
+      var l2 = gl.getUniformLocation(this.shaderProgram, "time");
+      var l3 = gl.getUniformLocation(this.shaderProgram, "resolution");
       
-      var t0 = gl.getUniformLocation(this.mProgram, "tex0");
+      var t0 = gl.getUniformLocation(this.shaderProgram, "tex0");
       
       gl.bindBuffer(gl.ARRAY_BUFFER, this.quadVBO);
       if( l2!=null ) gl.uniform1f(l2, time);
       if( l3!=null ) gl.uniform2f(l3, this.width, this.height);
       
-      
       gl.vertexAttribPointer(l1, 2, gl.FLOAT, false, 0, 0);
       
       gl.enableVertexAttribArray(l1);
       
-     // if( t0!=null ) { gl.uniform1i(t0, 0 ); gl.activeTexture(gl.TEXTURE0); gl.bindTexture(gl.TEXTURE_2D, this.mTexture); }
+      if( t0!=null ) { gl.uniform1i(t0, 0 ); gl.activeTexture(gl.TEXTURE0); gl.bindTexture(gl.TEXTURE_2D, this.mTexture); }
       
       gl.drawArrays(gl.TRIANGLES, 0, 6);
       gl.disableVertexAttribArray(l1);
-      // console.log('?');
     }
     
   };
   
-})();*/
+})();
+
