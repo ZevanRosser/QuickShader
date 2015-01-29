c6ddf143e5e9119bd6d47ed8a06ff8022abb3ad8
ZevanRosser <bendvent@gmail.com>
2015-01-29 15:58:54 -0500

    about to hack some thing up

--- src/quick-shader.js
+++ src/quick-shader.js
@@ -35,6 +35,19 @@
     
     this.init();
     
+    /*
+     // Create the canvas element, compile the shader code, and bind the XOR texture to the context.
+  var txt = document.getElementById( 'effectShader' ).textContent;
+  //document.write("<canvas id='x'></canvas>");
+  var can = document.createElement('canvas');
+  can.setAttribute('id', 'x');
+  document.body.appendChild(can);
+  var glCanvas = document.getElementById("x");
+  var glContext = glCanvas.getContext("experimental-webgl");  
+  var effect = new Effect( glContext, glCanvas.width, glCanvas.height );
+  effect.NewShader( txt );
+    */
+    
   };
   
   QuickShader.prototype = {
@@ -45,23 +58,40 @@
       var gl, vertices;
       
       this.canvas = document.createElement('canvas');
+      this.canvas.width = this.width;
+      this.canvas.height = this.height;
       gl = this.gl = this.ctx = this.canvas.getContext("experimental-webgl");
-      vertices = new Float32Array([ -1.0, -1.0, 1.0, -1.0, -1.0,  1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0 ]);
-      
+      vertices = new Float32Array([ -1., -1.,   1., -1.,    -1.,  1.,     1., -1.,    1.,  1.,    -1.,  1.]);
       this.quadVBO = gl.createBuffer();
       gl.bindBuffer(gl.ARRAY_BUFFER, this.quadVBO);
       gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
       
+      
+      
+      this.parentNode.appendChild(this.canvas);
       this.configureShader();
+      this.size(this.width, this.height);
+      
+      this.render(0);
+      
+      // Main loop.
+      var self = this;
+      var t0 = (new Date()).getTime();  
+      function animate() {
+        self.render( ((new Date()).getTime()-t0)/1000 );
+        requestAnimationFrame( animate );
+      }
+      animate();
     },
     
     size: function(width, height) {
       this.width = width;
       this.height = height;
+      this.gl.viewport( 0, 0, width, height);
     },
     
     configureShader: function() {
-      
+      console.log(this.shader);
       var gl = this.gl,
           tmpProgram = gl.createProgram(),
           vs = gl.createShader(gl.VERTEX_SHADER),
@@ -98,6 +128,7 @@
       if( this.mProgram != null ) gl.deleteProgram( this.mProgram );
       
       this.mProgram = tmpProgram;
+      console.log(this.mProgram);
     },
     
     render: function(time) {
@@ -113,18 +144,20 @@
       
       var t0 = gl.getUniformLocation(this.mProgram, "tex0");
       
-      gl.bindBuffer(gl.ARRAY_BUFFER, this.mQuadVBO);
+      gl.bindBuffer(gl.ARRAY_BUFFER, this.quadVBO);
       if( l2!=null ) gl.uniform1f(l2, time);
-      if( l3!=null ) gl.uniform2f(l3, this.mXres, this.mYres);
+      if( l3!=null ) gl.uniform2f(l3, this.width, this.height);
+      
       
       gl.vertexAttribPointer(l1, 2, gl.FLOAT, false, 0, 0);
       
       gl.enableVertexAttribArray(l1);
       
-      if( t0!=null ) { gl.uniform1i(t0, 0 ); gl.activeTexture(gl.TEXTURE0); gl.bindTexture(gl.TEXTURE_2D, this.mTexture); }
+     // if( t0!=null ) { gl.uniform1i(t0, 0 ); gl.activeTexture(gl.TEXTURE0); gl.bindTexture(gl.TEXTURE_2D, this.mTexture); }
       
       gl.drawArrays(gl.TRIANGLES, 0, 6);
       gl.disableVertexAttribArray(l1);
+      // console.log('?');
     }
     
   };
